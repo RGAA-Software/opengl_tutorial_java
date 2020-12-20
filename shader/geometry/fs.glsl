6ce5b0cd4961f1ee4bfa1531a67599aae6a8bf16
chessplayer <chessplayer@126.com>
2020-12-20 23:35:27 +0800

    add multi triangles
    Signed-off-by: chessplayer <chessplayer@126.com>

--- shader/geometry/fs.glsl
+++ shader/geometry/fs.glsl
@@ -1,7 +1,9 @@
 #version 330 core
 
+in vec4 color;
+
 void main() {
 
-    gl_FragColor = vec4(1.0, 0.5, 0, 1.0);
+    gl_FragColor = color;//vec4(1.0, 0.5, 0, 1.0);
 
 }
--- shader/geometry/gs.glsl
+++ shader/geometry/gs.glsl
@@ -1,17 +1,35 @@
 #version 330 core
 
 layout(points) in;
-layout(line_strip, max_vertices = 2) out;
+layout(triangle_strip, max_vertices = 5) out;
+
+out vec4 color;
 
 void main() {
-    gl_Position = gl_in[0].gl_Position + vec4(-0.1, 0, 0, 0);
-    // no use at line strip, only useful for point
-    //gl_PointSize = gl_in[0].gl_PointSize;
+//    gl_Position = gl_in[0].gl_Position + vec4(-0.1, 0, 0, 0);
+//    // no use at line strip, only useful for point
+//    //gl_PointSize = gl_in[0].gl_PointSize;
+//    EmitVertex();
+//
+//    gl_Position = gl_in[0].gl_Position + vec4(0.1, 0, 0, 0);
+//    //gl_PointSize = gl_in[0].gl_PointSize;
+//    EmitVertex();
+//
+//    EndPrimitive();
+
+    vec4 position = gl_in[0].gl_Position;
+    color = vec4(1, 0.5, 0.5, 1);
+    gl_Position = position + vec4(-0.2, -0.2, 0.0, 0.0);// 1:左下
+    EmitVertex();
+    gl_Position = position + vec4( 0.2, -0.2, 0.0, 0.0);    // 2:右下
+    EmitVertex();
+    gl_Position = position + vec4(-0.2,  0.2, 0.0, 0.0);    // 3:左上
+    EmitVertex();
+    gl_Position = position + vec4( 0.2,  0.2, 0.0, 0.0);    // 4:右上
     EmitVertex();
 
-    gl_Position = gl_in[0].gl_Position + vec4(0.1, 0, 0, 0);
-    //gl_PointSize = gl_in[0].gl_PointSize;
+    color = vec4(1, 1, 1, 1);
+    gl_Position = position + vec4( 0.0,  0.4, 0.0, 0.0);    // 5:顶部
     EmitVertex();
-
     EndPrimitive();
 }
