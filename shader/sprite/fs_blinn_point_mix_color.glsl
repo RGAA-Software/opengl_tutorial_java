857ec6b6fffac1b32a70aa254a906e1d164faedb
huayang-os <huayang-os@360os.com>
2021-01-14 11:49:13 +0800

    modify
    Signed-off-by: huayang-os <huayang-os@360os.com>

--- shader/sprite/fs_blinn_point_mix_color.glsl
+++ shader/sprite/fs_blinn_point_mix_color.glsl
@@ -23,12 +23,25 @@ void main()
     if (mixColor.r < 0.0001 && mixColor.g < 0.0001 && mixColor.b < 0.0001) {
         FragColor = texColor;
     } else {
+//        if (gl_FragCoord.x > 400 && gl_FragCoord.x < 600) {
+//            //FragColor = vec4(mix(texColor.xyz, vec3(outMixColor), 0.9), 1.0);
+//            float height = texture(imageHeight, outTex).r;
+//            if (height < 0) {
+//                height = 0;
+//            }
+//            vec4 samplerColor = texture(imageSampler, vec2(1.0 - height, 0.2));
+//            FragColor = vec4(mix(texColor.xyz, samplerColor.rgb, 0.7), 1.0);
+//        } else {
+//            FragColor = texColor;
+//        }
+
         //FragColor = vec4(mix(texColor.xyz, vec3(outMixColor), 0.9), 1.0);
         float height = texture(imageHeight, outTex).r;
         if (height < 0) {
             height = 0;
         }
         vec4 samplerColor = texture(imageSampler, vec2(1.0 - height, 0.2));
-        FragColor = vec4(mix(texColor.xyz, samplerColor.rgb, 0.9), 1.0);
+        FragColor = vec4(mix(texColor.xyz, samplerColor.rgb, 0.7), 1.0);
+
     }
 }
